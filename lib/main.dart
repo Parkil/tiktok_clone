f6d8f90312651c0aad2171a41d4dd719d253eaf7
Park il <alkain77@gmail.com>
2023-12-13 14:58:56 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'constants/gaps.dart';
+import 'constants/sizes.dart';
+
+void main() {
+  runApp(const TikTokApp());
+}
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TikTok Clone',
+      theme: ThemeData(
+        primaryColor: const Color(0xFFE9435A),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Padding(
+        padding: const EdgeInsets.all(Sizes.size14),
+        child: Container(
+          child: const Row(children: [
+            Text("say hello"),
+            Gaps.h20,
+            Text("hi"),
+          ]),
+        )
+      )
+    );
+  }
+}
+
+
